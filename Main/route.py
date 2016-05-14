7f87758f1fc808f30e6f9a34521afaa565ac2614
Patrick Lai <patrickcylai@gmail.com>
2016-05-14 22:02:23 +1000

    changed from python 3 to python 2.7, can now receive data over LAN in route file

--- Main/route.py
+++ Main/route.py
@@ -1,28 +1,34 @@
 from flask import *
-import ConfigParser
-from comms import *
+# import configparser
+# from comms import *
 import random
+import json
+import urllib2
+
 
 app = Flask(__name__)
-app.debug = True
+app.secret_key = 'hello'
 
 page = {}
 session = {}
 
 @app.route('/')
 def index():
-    page['title'] = 'Welcome'
+    page['title'] = 'Overview'
     page['bins'] = '1'
     page['users'] = str(random.randint(0,100))
     return render_template('index.html', page = page)
 
-@app.route('/capacity', methods=['POST','GET'])
+@app.route('/capacity', methods=['GET','POST'])
 def get_capacity():
+    # if urllib2.Request.get_method == 'GET':
+    res = urllib2.urlopen('http://192.168.1.49:80')
+    res = json.loads(res.read())
     page['title'] = 'Capacity'
     level = random.randint(0,100)
-    page['level'] = str(level)
-    page['weight'] = str(random.randint(0,10)) + 'kg'
-    page['distance'] = str(random.randint(0,50)) + 'cm'
+    page['level'] = res['Capacity']
+    page['weight'] = str(random.randint(0,10)) + ' kg'
+    page['distance'] = str(random.randint(0,50)) + ' cm'
     if level > 90:
         page['status'] = 'FULL'
     else:
