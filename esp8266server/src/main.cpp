fd69b3efdddbdc58662f920f66297d52db7b3706
Joshua Manogaran <josh.14668@gmail.com>
2016-05-20 16:52:36 +1000

    ESP Server code edited with TWI for US sensor and JSON output

--- esp8266server/src/main.cpp
+++ esp8266server/src/main.cpp
@@ -1,31 +1,44 @@
-/*
- *  This sketch demonstrates how to scan WiFi networks.
- *  The API is almost the same as with the WiFi Shield library,
- *  the most obvious difference being the different file you need to include:
- */
+
 #include <ESP8266WiFi.h>
 
 #include <WiFiClient.h>
 #include <ESP8266WebServer.h>
+#include <Wire.h>
+#define I2CAddress 8
+
 
 String webString = "";
 int distance;
+float weight;
 int prevDist=0;
 
 WiFiServer server(80);
 
-const char* ssid     = "TPPW4G_D0D5";
-const char* password = "24409193";
+//Put SSID and Password here
+const char* ssid     = "";
+const char* password = "";
 
 
 
 void getDistance(){
-  distance=Serial.read();
+  Wire.requestFrom(I2CAddress, 1);
+
+  while(Wire.available()){
+    distance = Wire.read();
+    Serial.println(distance);
+  }
 }
 
 void setup() {
-  Serial.println("Hello");
-  Serial.begin(115200);
+
+  Serial.begin(9600);
+
+  Wire.pins(SDA,SCL);//TWI for sensor
+  Wire.begin(SDA,SCL);
+
+
+
+
   WiFi.begin(ssid,password);
   Serial.print("\n\r \n\r working to connect");
 
@@ -40,13 +53,6 @@ void setup() {
   Serial.print("IP address: ");
   Serial.println(WiFi.localIP());
 
-
-  // server.on("/", handle_root);
-  // server.on("/distance", [](){
-  //   getDistance();
-  //   webString="Distance: "+String((int)distance)+"cm";
-  //   server.send(200,"text/plain",webString);
-  // });
   server.begin();
   Serial.println("HTTP server started");
 }
@@ -54,31 +60,43 @@ void setup() {
 void loop(){
   getDistance();
 
-  Serial.println(distance);
   WiFiClient client = server.available();
   client.println("HTTP/1.1 200 OK");
   client.println("Content-Type: text/html");
   client.println("Connection: close");  // the connection will be closed after completion of the response
-  
+
 
 
   client.println("Refresh: 5");  // refresh the page automatically every 5 sec
 
 
   client.println();
-  client.println("<!DOCTYPE html>");
-  client.println("<html xmlns='http://www.w3.org/1999/xhtml'>");
-  client.println("<head>\n<meta charset='UTF-8'>");
-  client.println("<title>ESP8266 Distance Sensor</title>");
-  client.println("</head>\n<body>");
-  client.println("<H2>ESP8266 & HC-SR04 Sensor</H2>");
-  client.println("<H3>Distance </H3>");
-  client.println("<pre>");
+  // client.println("<!DOCTYPE html>");
+  // client.println("<html xmlns='http://www.w3.org/1999/xhtml'>");
+  // client.println("<head>\n<meta charset='UTF-8'>");
+  // client.println("<title>ESP8266 Distance Sensor</title>");
+  // client.println("</head>\n<body>");
+  // client.println("<H2>ESP8266 & HC-SR04 Sensor</H2>");
+  // client.println("<H3>Distance </H3>");
+  // client.println("<pre>");
+  //
+  // client.print("Distance (cm)  : ");
+  // client.println((int)distance);
 
-  client.print("Distance (cm)  : ");
-  client.println((int)distance);
+//JSON Data
+  client.print("{\"level\":");
+  client.print(3);//Dummy Number
+  client.print(",");
+  client.print("\"weight\":");
+  client.print(5);//Dummy Number
+  client.print(",");
+  client.print("\"distance\":");
+  client.print(distance);
+  client.print("}");
+  client.println();
 
-  client.print("</body>\n</html>");
+
+
   delay(1000);
 
 }
