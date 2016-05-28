7fc348a76093a48a088239d4463e69161773298e
Joshua Manogaran <josh.14668@gmail.com>
2016-05-28 12:40:10 +1000

    Updated esp8266 server code

--- esp8266server/src/main.cpp
+++ esp8266server/src/main.cpp
@@ -1,6 +1,10 @@
+/*
+ *  This is the code the ESP8266 wifi module uses to read
+ *  sensor values via I2C from the Arduino Due as well as connect
+ *  to the internet and send the values in JSON to another webserver
+ */
 
 #include <ESP8266WiFi.h>
-
 #include <WiFiClient.h>
 #include <ESP8266WebServer.h>
 #include <Wire.h>
@@ -8,25 +12,67 @@
 
 
 String webString = "";
-int distance;
-float weight;
-int prevDist=0;
+
+byte buff[10];
+int rawWeight;
+int weight;
+int rawLevel;
+int level;
+int user;
+
+
+
 
 WiFiServer server(80);
 
-//Put SSID and Password here
-const char* ssid     = "";
-const char* password = "";
+//SSID and Password of wifi go here
+const char* ssid     = "TPPW4G_D0D5";
+const char* password = "24409193";
 
-
-
+//Function that requests data from the Due via I2C
 void getDistance(){
-  Wire.requestFrom(I2CAddress, 1);
+  // Master request Code for Two Wire Interface, ESP master
+  int n = Wire.requestFrom(I2CAddress, 4);
 
-  while(Wire.available()){
-    distance = Wire.read();
-    Serial.println(distance);
+
+  //Reads data byte by byte and stores in array
+  for(int i=0;i<n;i++){
+    buff[i]=Wire.read();
   }
+
+  //Check to make sure weight sensor value is within a good range
+  if(buff[1]<100 && buff[1]>=0){
+    rawLevel=buff[1];
+  }
+  rawWeight=buff[0];
+
+  /*Users are numbered from 1 to 4 however this vlaue
+  is multiplied by 10 when sent from the due. This is
+  done to reduce effect of random data in I2c interface.
+  */
+
+  switch (buff[2]){
+    case 0:
+      user=0;
+      break;
+    case 20:
+      user = 1;
+      break;
+    case 40:
+      user = 2;
+      break;
+    case 60:
+      user = 3;
+    case 80:
+      user=4;
+      break;
+    case 100:
+      user=5;
+      break;
+    }
+
+
+
 }
 
 void setup() {
@@ -60,6 +106,30 @@ void setup() {
 void loop(){
   getDistance();
 
+  //Smoothing function for level and weight
+  level=(0.8*level)+(0.2*rawLevel);
+
+  weight=(0.8*weight)+(0.2*rawWeight);
+
+
+  // if(rawDistance<100){
+  //   distance=rawDistance;
+  // }
+
+
+
+  // Serial.println("Distance:");
+  // Serial.println(distance);
+
+  Serial.print("Weight:");
+  Serial.println(weight);
+  Serial.print("Level:");
+  Serial.println(level);
+  Serial.print("User:");
+  Serial.println(user);
+
+
+
   WiFiClient client = server.available();
   client.println("HTTP/1.1 200 OK");
   client.println("Content-Type: text/html");
@@ -83,15 +153,15 @@ void loop(){
   // client.print("Distance (cm)  : ");
   // client.println((int)distance);
 
-//JSON Data
+  //JSON Data written to webserver
   client.print("{\"level\":");
-  client.print(3);//Dummy Number
+  client.print(level);
   client.print(",");
   client.print("\"weight\":");
-  client.print(5);//Dummy Number
+  client.print(weight);
   client.print(",");
-  client.print("\"distance\":");
-  client.print(distance);
+  client.print("\"user\":");
+  client.print(user);
   client.print("}");
   client.println();
 
